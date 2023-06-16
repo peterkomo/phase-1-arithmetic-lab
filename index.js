f0706f22be2931c37a36f88221cd7e5a87af9cc6
peterkomo <mwangipeterkomo@gmail.com>
2023-06-16 11:41:58 +0300

    will make nessesary chnges once thought off

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+const num1=31
+const num2=2
+const multiply=num1*num2
+let random=Math.floor(Math.random()*10)+2;
+
+let num3=64
+let num4=10
+let mod=(num3%num4)
+
+
+Math.max(10,5,6,72,6,20)
+console.log(Math.max)
